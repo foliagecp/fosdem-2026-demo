0ff265a8cc6e335237cf8df0b31cc76c239beed4
atauov <atauov@gmail.com>
2026-01-22 20:28:02 +0500

    1

--- m1/adapters/infra/main.go
+++ m1/adapters/infra/main.go
@@ -14,7 +14,6 @@ import (
 	statefun "github.com/foliagecp/sdk/statefun"
 	"github.com/foliagecp/sdk/statefun/cache"
 	lg "github.com/foliagecp/sdk/statefun/logger"
-	sfPlugins "github.com/foliagecp/sdk/statefun/plugins"
 	"github.com/foliagecp/sdk/statefun/system"
 )
 
@@ -34,7 +33,7 @@ var (
 
 func registerFunctionTypes(runtime *statefun.Runtime) {
 	statefun.NewFunctionType(runtime, pushUpdateFnName, infraPushUpdate, *statefun.NewFunctionTypeConfig())
-	statefun.NewFunctionType(runtime, postProcessingFnName, infraPostProcess, *statefun.NewFunctionTypeConfig().SetAllowedSignalProviders(sfPlugins.AutoSignalSelect))
+	statefun.NewFunctionType(runtime, postProcessingFnName, infraPostProcess, *statefun.NewFunctionTypeConfig())
 }
 
 func onAfterStart(ctx context.Context, runtime *statefun.Runtime) error {
