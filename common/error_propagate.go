e411456f64c3be5bd466116553f6484cb32d9294
atauov <atauov@gmail.com>
2026-01-27 12:21:20 +0500

    add manual actions runs

--- common/error_propagate.go
+++ common/error_propagate.go
@@ -20,7 +20,6 @@ const PropagateErrorFunctionName = "functions.common.propagate_error"
 // "__error_timestamp_nano" int
 
 func PropagateError(_ sfPlugins.StatefunExecutor, ctx *sfPlugins.StatefunContextProcessor) {
-	lg.Logf(lg.DebugLevel, "PropagateError() called on: %v", ctx.Self.ID)
 	var errorTS int64
 	errorTSFloat, ok := ctx.Payload.GetByPath("body.__error_timestamp_nano").AsNumeric()
 	if !ok {
@@ -31,19 +30,20 @@ func PropagateError(_ sfPlugins.StatefunExecutor, ctx *sfPlugins.StatefunContext
 
 	dbc, err := db.NewDBSyncClientFromRequestFunction(ctx.Request)
 	if err != nil {
-		lg.Logf(lg.ErrorLevel, "Error propagating error: %s", err.Error())
+		lg.Logf(lg.ErrorLevel, "PropagateError: cant create db sync client: %v", err)
 		return
 	}
 
 	currentObject, err := dbc.CMDB.ObjectRead(ctx.Self.ID)
 	if err != nil {
-		lg.Logf(lg.ErrorLevel, "Error propagating error: %s", err.Error())
+		lg.Logf(lg.ErrorLevel, "PropagateError: cant read object: %s", err)
 		return
 	}
 
 	if currentObject.PathExists("body.error.error") || currentObject.PathExists("body.error.error_distribution") {
 		existingTSFloat := currentObject.GetByPath("body.error.__error_timestamp_nano").AsNumericDefault(0)
 		if int64(existingTSFloat) >= errorTS {
+			lg.Logf(lg.DebugLevel, "PropagateError: existing error on object %s newer than propagating (%.0f > %d)", ctx.Self.ID, existingTSFloat, errorTS)
 			return
 		}
 	}
@@ -51,12 +51,14 @@ func PropagateError(_ sfPlugins.StatefunExecutor, ctx *sfPlugins.StatefunContext
 	propagateErrorPayload := easyjson.NewJSONObject()
 	propagateErrorPayload.SetByPath("error.distribution", easyjson.NewJSON(true))
 	propagateErrorPayload.SetByPath("error.__error_timestamp_nano", easyjson.NewJSON(errorTS))
-	system.MsgOnErrorReturn(dbc.CMDB.ObjectUpdate(ctx.Self.ID, propagateErrorPayload, false))
+	if err = dbc.CMDB.ObjectUpdate(ctx.Self.ID, propagateErrorPayload, false); err != nil {
+		lg.Logf(lg.ErrorLevel, "PropagateError: cant update object %s: %v", ctx.Self.ID, err)
+	}
 
 	for _, tag := range ErrorPropagateLinkTags {
 		query := fmt.Sprintf(".*[l:tags('%s')]", tag)
 		linkedIDs, err := dbc.Query.JPGQLCtraQuery(ctx.Self.ID, query)
-		lg.Logf(lg.DebugLevel, "PropagateError: linkedIDs: %v", linkedIDs)
+		lg.Logf(lg.DebugLevel, "PropagateError: linkedIDs for %s: %v", ctx.Self.ID, linkedIDs)
 		if err == nil && len(linkedIDs) > 0 {
 			propagateToLinked(ctx, linkedIDs)
 			return
